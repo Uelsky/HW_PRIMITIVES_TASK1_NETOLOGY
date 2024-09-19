046fe2cf1f199c27423721311e3428094aa822ec
Uelsky <maktavish123@gmail.com>
2024-09-19 15:50:36 +0300

    modified: src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Input flight cost: ");
-        int amount = in.nextInt();
+
+        System.out.print("Input flight price: ");
+        int price = in.nextInt();
         int coin = 20;
-        int bonusMiles = amount / coin;
+
+        int bonusMiles = price / coin;
         System.out.println("Your bonus miles: " + bonusMiles);
 
+        in.close();
     }
 }
